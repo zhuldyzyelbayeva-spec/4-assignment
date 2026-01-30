671500650eea6a3b67f3a225ad0ece8b505fb91d
zhuldyzyelbayeva-spec <zhuldyzyelbayeva@gmail.com>
2026-01-30 16:53:09 +0500

    Update ReflectionUtils.java

--- src/utils/ReflectionUtils.java
+++ src/utils/ReflectionUtils.java
@@ -1,1 +1,20 @@
 
+package utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectionUtils {
+
+    public static void printClassInfo(Class<?> clazz) {
+        System.out.println("Class: " + clazz.getName());
+
+        for (Field f : clazz.getDeclaredFields()) {
+            System.out.println("Field: " + f.getName());
+        }
+
+        for (Method m : clazz.getDeclaredMethods()) {
+            System.out.println("Method: " + m.getName());
+        }
+    }
+}
